3826acee91f064d0f33e11330fa18d5303e355b4
杜鹏 <dupeng.tommy@outlook.com>
2018-07-27 21:24:33 +0800

    首次提交

---
+++ pages/center/center.js
@@ -0,0 +1,76 @@
+// pages/center.js
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    Industry: {},
+    xuexiao:""
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function(options) {
+    var that = this;
+    wx.request({
+      url: 'https://lebaobao.azurewebsites.net/class/GetClassList',
+      success: function(res) {
+        console.log(res.data.classList);
+
+        that.setData({
+          Industry: res.data.classList,
+          xuexiao: res.data.classList[1].Description
+        });
+      }
+    })
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function() {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function() {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function() {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function() {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function() {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function() {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function() {}
+})
