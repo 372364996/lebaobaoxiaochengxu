c0723e7a28f14e3b9ad6671952cbde13792fbae8
杜鹏 <dupeng.tommy@outlook.com>
2018-10-11 00:28:38 +0800

    +新增到访记录入口
    新增到访记录入口

--- pages/center/center.js
+++ pages/center/center.js
@@ -1,96 +1,97 @@
-// pages/center.js
+//index.js
+//获取应用实例
+var app = getApp()
 Page({
+  data: {
+    motto: '邯郸乐宝宝小儿推拿',
+    userInfo: {},
+    childName: null,
+    phone: null,
+    canusecount: 0,
+    current: 'homepage'
+  },
 
-    /**
-     * 页面的初始数据
-     */
-    data: {
-        Industry: {},
-        xuexiao: "",
-        markers: [{
-            id: 1,
-            latitude: 36.577757,
-            longitude: 114.491674,
-            name: '邯郸乐宝宝小儿推拿'
-        }]
-    },
-    handleChange({
-        detail
-    }) {
-        this.setData({
-            current: detail.key
-        });
-        if (detail.key == "homepage") {
-            wx.navigateTo({
-                url: '../index/index',
-            })
+  handleChange({
+    detail
+  }) {
+    this.setData({
+      current: detail.key
+    });
+    if (detail.key=="homepage"){
+      wx.navigateTo({
+        url: '../index/index',
+      })
+      
+    }
+    else if (detail.key == "mine") {
+      wx.navigateTo({
+        url: '../center/center',
+      })
+    }
+  },
+  //事件处理函数
+  bindViewTap: function() {
+    wx.navigateTo({
+      url: '../logs/logs'
+    })
+  },
+  goToCenter: function() {
+    wx.navigateTo({
+      url: '../center/center',
+    })
+  },
+  onGotUserInfo: function(e) {
+    console.log(e.detail.errMsg)
+    console.log(e.detail.userInfo)
+    console.log(e.detail.rawData)
+  },
+  onLoad: function() {
+    console.log('onLoad')
+    console.log(app.globalData)
+    var that = this
+    //调用应用实例的方法获取全局数据
 
-        } else if (detail.key == "mine") {
-            wx.navigateTo({
-                url: '../center/center',
+    wx.login({
+      success: function(res_code) {
+        wx.getUserInfo({
+          withCredentials: true,
+          success: function(res) {
+            that.setData({
+              userInfo: res.userInfo
             })
-        }
-    },
-    /**
-     * 生命周期函数--监听页面加载
-     */
-    onLoad: function(options) {
-        var that = this;
-        wx.request({
-            url: 'https://lebaobao.azurewebsites.net/class/GetClassList',
-            success: function(res) {
-                that.setData({
-                    Industry: res.data.classList,
-                    xuexiao: res.data.classList[1].Description
+            wx.request({
+              url: app.globalData.hostUrl + '/weichat/login',
+              data: {
+                code: res_code.code,
+                encryptedData: res.encryptedData,
+                iv: res.iv
+              },
+              success: function(res_user) {
+                wx.request({
+                  url: app.globalData.hostUrl + '/weichat/Info',
+                  data: {
+                    userid: res_user.data.Id
+                  },
+                  success: function(res) {
+                    that.setData({
+                      childName: res.data.ChildName,
+                      phone: res.data.Phone,
+                      canusecount: res.data.CanUseCount
+                    });
+                  }
                 });
-            }
+                console.log(res_user.data.Id)
+              }
+            })
+          },
+          fail: function() {
+            //获取用户信息失败后。请跳转授权页面         
+            wx.navigateTo({
+              url: '../login/login',
+            })
+          }
         })
-    },
-
-    /**
-     * 生命周期函数--监听页面初次渲染完成
-     */
-    onReady: function() {
-
-    },
-
-    /**
-     * 生命周期函数--监听页面显示
-     */
-    onShow: function() {
-
-    },
-
-    /**
-     * 生命周期函数--监听页面隐藏
-     */
-    onHide: function() {
-
-    },
-
-    /**
-     * 生命周期函数--监听页面卸载
-     */
-    onUnload: function() {
-
-    },
-
-    /**
-     * 页面相关事件处理函数--监听用户下拉动作
-     */
-    onPullDownRefresh: function() {
-
-    },
-
-    /**
-     * 页面上拉触底事件的处理函数
-     */
-    onReachBottom: function() {
-
-    },
-
-    /**
-     * 用户点击右上角分享
-     */
-    onShareAppMessage: function() {}
+      }
+    })
+  }
 })
--- pages/index/index.js
+++ pages/index/index.js
@@ -1,97 +1,116 @@
-//index.js
+// pages/center.js
 //获取应用实例
 var app = getApp()
 Page({
-  data: {
-    motto: '邯郸乐宝宝小儿推拿',
-    userInfo: {},
-    childName: null,
-    phone: null,
-    canusecount: 0,
-    current: 'homepage'
-  },
 
-  handleChange({
-    detail
-  }) {
-    this.setData({
-      current: detail.key
-    });
-    if (detail.key=="homepage"){
-      wx.navigateTo({
-        url: '../index/index',
-      })
-      
-    }
-    else if (detail.key == "mine") {
-      wx.navigateTo({
-        url: '../center/center',
-      })
-    }
-  },
-  //事件处理函数
-  bindViewTap: function() {
-    wx.navigateTo({
-      url: '../logs/logs'
-    })
-  },
-  goToCenter: function() {
-    wx.navigateTo({
-      url: '../center/center',
-    })
-  },
-  onGotUserInfo: function(e) {
-    console.log(e.detail.errMsg)
-    console.log(e.detail.userInfo)
-    console.log(e.detail.rawData)
-  },
-  onLoad: function() {
-    console.log('onLoad')
-    console.log(app.globalData)
-    var that = this
-    //调用应用实例的方法获取全局数据
-
-    wx.login({
-      success: function(res_code) {
-        wx.getUserInfo({
-          withCredentials: true,
-          success: function(res) {
-            that.setData({
-              userInfo: res.userInfo
-            })
-            wx.request({
-              url: app.globalData.hostUrl + '/weichat/login',
-              data: {
-                code: res_code.code,
-                encryptedData: res.encryptedData,
-                iv: res.iv
-              },
-              success: function(res_user) {
-                wx.request({
-                  url: app.globalData.hostUrl + '/weichat/Info',
-                  data: {
-                    userid: res_user.data.Id
-                  },
-                  success: function(res) {
-                    that.setData({
-                      childName: res.data.ChildName,
-                      phone: res.data.Phone,
-                      canusecount: res.data.CanUseCount
-                    });
-                  }
-                });
-                console.log(res_user.data.Id)
-              }
-            })
-          },
-          fail: function() {
-            //获取用户信息失败后。请跳转授权页面         
-            wx.navigateTo({
-              url: '../login/login',
-            })
-          }
+    /**
+     * 页面的初始数据
+     */
+    data: {
+        Industry: {},
+        xuexiao: "",
+        markers: [{
+            id: 1,
+            latitude: 36.577757,
+            longitude: 114.491674,
+            name: '邯郸乐宝宝小儿推拿'
+        }]
+    },    
+    /**
+     * 生命周期函数--监听页面加载
+     */
+    onLoad: function(options) {
+        console.log('onLoad')
+        console.log(app.globalData)
+        var that = this
+        wx.login({
+            success: function (res_code) {
+                wx.getUserInfo({
+                    withCredentials: true,
+                    success: function (res) {
+                        that.setData({
+                            userInfo: res.userInfo
+                        })
+                        wx.request({
+                            url: app.globalData.hostUrl + '/weichat/login',
+                            data: {
+                                code: res_code.code,
+                                encryptedData: res.encryptedData,
+                                iv: res.iv
+                            },
+                            success: function (res_user) {
+                                wx.request({
+                                    url: app.globalData.hostUrl + '/weichat/Info',
+                                    data: {
+                                        userid: res_user.data.Id
+                                    },
+                                    success: function (res) {
+                                        that.setData({
+                                            childName: res.data.ChildName,
+                                            phone: res.data.Phone,
+                                            canusecount: res.data.CanUseCount
+                                        });
+                                    }
+                                });
+                                console.log(res_user.data.Id)
+                            }
+                        })
+                    },
+                    fail: function () {
+                        //获取用户信息失败后。请跳转授权页面         
+                        wx.navigateTo({
+                            url: '../login/login',
+                        })
+                    }
+                })
+            }
         })
-      }
-    })
-  }
+    },
+
+    /**
+     * 生命周期函数--监听页面初次渲染完成
+     */
+    onReady: function() {
+
+    },
+
+    /**
+     * 生命周期函数--监听页面显示
+     */
+    onShow: function() {
+
+    },
+
+    /**
+     * 生命周期函数--监听页面隐藏
+     */
+    onHide: function() {
+
+    },
+
+    /**
+     * 生命周期函数--监听页面卸载
+     */
+    onUnload: function() {
+
+    },
+
+    /**
+     * 页面相关事件处理函数--监听用户下拉动作
+     */
+    onPullDownRefresh: function() {
+
+    },
+
+    /**
+     * 页面上拉触底事件的处理函数
+     */
+    onReachBottom: function() {
+
+    },
+
+    /**
+     * 用户点击右上角分享
+     */
+    onShareAppMessage: function() {}
 })
